d6c840893cdc29173403efabce93498e1d753262
Carlos Antunes <carlos.antunes@ik.me>
2024-11-07 00:58:18 +0100

    Lab 2 - Tâche 1 : Singleton pattern - Retour Yassin Refactor State pattern avec la récupération du context et refactor les tests

--- src/main/java/com/carlosantunes/restaurant/etat/Reserver.java
+++ src/main/java/com/carlosantunes/restaurant/etat/Reserver.java
@@ -5,36 +5,32 @@ import com.carlosantunes.restaurant.Table;
 public class Reserver extends TableState {
     private static final Reserver instance = new Reserver();
 
-    private Reserver() {}
-
-    public static Reserver getInstance() {
+     public static Reserver getInstance() {
         return instance;
     }
 
     @Override
-    public TableState accueillirClient(Table table) {
+    public void accueillirClient(Table table) {
         System.out.println("Le client est accueilli à la table " + table.getTableType());
-        return Servie.getInstance();
+        table.setEtatDeLaTable(Servie.getInstance());
     }
 
     @Override
-    public TableState servirProduits(Table table) {
+    public void servirProduits(Table table) {
         try {
             afficheErreur("Erreur : Impossible de servir des produits. La table est réservée et le client n'est pas encore accueilli.");
         } catch (IllegalStateException e) {
             System.out.println("Message : " + e.getMessage());
         }
-        return this;
     }
 
     @Override
-    public TableState fermer(Table table) {
+    public void fermer(Table table) {
         try {
             afficheErreur("Erreur : Impossible de fermer la table. La table est réservée et le client n'est pas encore accueilli.");
         } catch (IllegalStateException e) {
             System.out.println("Message : " + e.getMessage());
         }
-        return this;
     }
 
     @Override
